43127162176b7c98a698ac62c6a20411b392e024
RanaOsamaAwadallah <rana_osama_awadallah@yahoo.com>
2018-05-19 23:07:22 +0200

    User html and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { UserProfileComponent } from './user/user-profile/user-profile.component';
+import { UserDashBoardComponent } from './user/user-dash-board/user-dash-board.component';
+import { AppComponent } from './app.component';
+
+const appRoutes: Routes = [
+  // {path: '', redirectTo: '/home', pathMatch: 'full'},
+  // {path:'/home', component: AppComponent},
+  {path:'/userProfile', component: UserProfileComponent},
+  {path:'/userDashBoard', component:UserDashBoardComponent}
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  declarations: []
+  declarations: [],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { UserModule } from './user/user.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // AppRoutingModule,
+    UserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
