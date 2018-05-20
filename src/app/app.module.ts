d9bbd658dec766d9c28d0c86450dfb8729be0dcd
soaad <booosyosama@gmail.com>
2018-05-20 14:31:06 +0200

    fix in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,9 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { UserModule } from './user/user.module';
-<<<<<<< HEAD
+
 import { InterviewsModule } from 'src/app/interviews/interviews.module';
-=======
 import { EnterpriseService } from './shared/classes/enterprise-service';
->>>>>>> b8420855242d733421c48e62db6d2a5e8bd2c4c3
 
 @NgModule({
   declarations: [
@@ -17,15 +15,14 @@ import { EnterpriseService } from './shared/classes/enterprise-service';
   ],
   imports: [
     BrowserModule,
-<<<<<<< HEAD
+
     // AppRoutingModule,
     // UserModule,
-    InterviewsModule
-=======
+    InterviewsModule,
     AppRoutingModule,
     UserModule,
     EnterprisesModule
->>>>>>> 9329685192655e688031c129c4a367bd5c783ae4
+
   ],
   providers: [EnterpriseService],
   bootstrap: [AppComponent]
