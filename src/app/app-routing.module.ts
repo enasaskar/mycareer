6569425a682f9f8291e56c644aa7cff997fb8890
RanaOsamaAwadallah <rana_osama_awadallah@yahoo.com>
2018-05-21 10:26:09 +0200

    user css added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const appRoutes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path:'home', component: AppComponent},
   {path:'userProfile', component: UserProfileComponent},
-  {path:'userDashBoard', component:UserDashBoardComponent}
+  {path:'userDashBoard', component: UserDashBoardComponent}
 ];
 
 @NgModule({
