f3a8a6ff7db58afe0c1bd87deb6039fdf13977c6
RanaOsamaAwadallah <rana_osama_awadallah@yahoo.com>
2018-05-23 10:56:22 +0200

    user modified

---
+++ src/app/users/users.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { UserDetailsEditComponent } from './user-details-edit/user-details-edit.component';
+import { UserDashBoardComponent } from './user-dash-board/user-dash-board.component';
+import { UserViewAllComponent } from './user-view-all/user-view-all.component';
+import { UsersComponent } from './users.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    UserProfileComponent, 
+    UserDetailsComponent, 
+    UserDetailsEditComponent, 
+    UserDashBoardComponent, 
+    UserViewAllComponent, UsersComponent
+  ],
+  exports: [
+    UserProfileComponent, 
+    UserDetailsComponent, 
+    UserDetailsEditComponent, 
+    UserDashBoardComponent, 
+    UserViewAllComponent
+  ]
+})
+export class UsersModule { }
