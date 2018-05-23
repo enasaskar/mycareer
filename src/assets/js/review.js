610c3ede83d20357de9c885699874efa7130bcbd
soaad <booosyosama@gmail.com>
2018-05-23 13:51:39 +0200

    add review.js

---
+++ src/assets/js/review.js
@@ -0,0 +1,6 @@
+// with plugin options (do not attach the CSS class "rating" to your input if using this approach)
+$(".input-active").rating({showCaption: false,
+ step:0.1});
+$(".input-disabled").rating({
+	displayOnly: true
+});
