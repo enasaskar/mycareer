a460765fab6685bed5a81bd38fa91656fdb0b53b
soaad <booosyosama@gmail.com>
2018-05-25 13:48:44 +0200

    bug fix app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,12 +13,11 @@ import { PathsModule } from './paths/path.module';
 import { EnterpriseService } from './shared/classes/enterprise-service';
 import { CoreModule } from 'src/app/core/core.module';
 import { SkillsModule } from './skills/skills.module';
-<<<<<<< HEAD
+
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-=======
+
 import { PathService } from './shared/services/path.service';
 import { PathCategoryService } from './shared/services/PathCategory.service';
->>>>>>> 678303212b9483e7272d2e6f4c1f2ff14d4b4646
 
 @NgModule({
   declarations: [
