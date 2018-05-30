75140cf0decdbad6fe7d412d7f892c6741ecd066
RanaOsamaAwadallah <rana_osama_awadallah@yahoo.com>
2018-05-30 11:19:37 +0200

    missing files added

---
+++ src/app/interviews/user-pending-interviews/user-pending-interviews.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-pending-interviews',
+  templateUrl: './user-pending-interviews.component.html',
+  styleUrls: ['./user-pending-interviews.component.css']
+})
+export class UserPendingInterviewsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
