2f9fe1e228d469302036dbbf671251d181d0de38
RanaOsamaAwadallah <rana_osama_awadallah@yahoo.com>
2018-06-05 11:39:08 +0200

    delete work experience in user profile added

--- src/app/enterprise/work-experience/work-exp-item/work-exp-item.component.ts
+++ src/app/enterprise/work-experience/work-exp-item/work-exp-item.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { EnterpriseService } from '../../../shared/services/enterprise.service';
 import { Enterprise } from '../../../shared/classes/enterprise';
+import { WorkExperienceService } from '../../../shared/services/workExperience.service';
+import { EducationalBackgroundService } from '../../../shared/services/educationalBackground.service';
 
 @Component({
   selector: 'app-work-exp-item',
@@ -10,12 +12,25 @@ import { Enterprise } from '../../../shared/classes/enterprise';
 export class WorkExpItemComponent implements OnInit {
 
   @Input() workExpItem;
-  @Input() enterprise: Enterprise;
-  constructor(private enterpriseService: EnterpriseService) { }
+  // @Input() enterprise: Enterprise;
+  @Input() workExpIndex: number;
+  @Input() type: string;
+  enterprise: Enterprise;
+
+  constructor(private enterpriseService: EnterpriseService,
+    private workExpService: WorkExperienceService,
+    private eduService: EducationalBackgroundService) { }
 
   ngOnInit() {
-    // this.entreprise = this.enterpriseService.getEnterpriseById(this.workExpItem.enterpriseID);
-    // console.log(this.entreprise);
+    this.enterprise = this.enterpriseService.getEnterpriseById(this.workExpItem.enterpriseID);
   }
 
+  deleteExperience() {
+    if (this.type === 'edu') {
+      this.eduService.deleteEducation(0, this.workExpIndex);
+
+    } else {
+      this.workExpService.deleteExperience(0, this.workExpIndex);
+    }
+  }
 }
