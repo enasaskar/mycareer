324a9da0701a2ed27e7cad8eac90cb82b759301f
soaad <booosyosama@gmail.com>
2018-06-06 16:01:08 +0200

    user add comment fn added

--- src/app/shared/services/user-comments.service.ts
+++ src/app/shared/services/user-comments.service.ts
@@ -1,13 +1,27 @@
-import { Injectable } from '@angular/core';
+import { Injectable,EventEmitter } from '@angular/core';
 import { UserComments } from '../classes/user-comments';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserCommentsService {
+
+  commentsChanged = new EventEmitter<UserComments[]>();
   private comments : UserComments[] = [
     {
       id:1,
+      userId:0,
+      enterpriseId:1,
+      comment:'I worked at this company for five years and it was a very good experience and environment'
+    },
+    {
+      id:2,
+      userId:0,
+      enterpriseId:1,
+      comment:'I worked at this company for five years and it was a very good experience and environment'
+    },
+    {
+      id:3,
       userId:0,
       enterpriseId:1,
       comment:'I worked at this company for five years and it was a very good experience and environment'
@@ -18,17 +32,12 @@ constructor() {
 }
 
     public getByEnterpriseId(id : number){
-      let enterpriseComments : UserComments[] = [];
-      for(let i = 0; i < this.comments.length; i++){
-        if(this.comments[i].enterpriseId == id){
-          enterpriseComments.push(this.comments[i]);
-        }
-        return enterpriseComments;
-      }
+      return this.comments.filter(a => a.enterpriseId == id);
     }
 
     public addComment(comment : UserComments){
         this.comments.push(comment);
+        this.commentsChanged.emit(this.getByEnterpriseId(comment.enterpriseId));
     }
 
 }
