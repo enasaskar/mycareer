f4d43b5150a8f995cf9e1f7cdf96503609c1a236
Amal Youssef <amalyoussef80@yahoo.com>
2018-06-19 03:00:23 +0200

    details done vacacy

--- src/app/vacancies/vacancy-item-details/vacancy-item-details.component.ts
+++ src/app/vacancies/vacancy-item-details/vacancy-item-details.component.ts
@@ -5,6 +5,11 @@ import { Enterprise } from '../../shared/classes/enterprise';
 import { EnterpriseService } from '../../shared/services/enterprise.service';
 import { NgForm } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { VacancyTypeService } from '../../shared/services/vacancyType-service';
+import { VacancyLevelService } from '../../shared/services/vacancyLevel-service';
+import { VacancyType } from '../../shared/classes/VacancyType';
+import { EnterpriseDetails } from '../../shared/classes/enterprise-details';
+import { VacancyLevel } from '../../shared/classes/vacancyLevel';
 
 @Component({
   selector: 'app-vacancy-item-details',
@@ -13,19 +18,29 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class VacancyItemDetailsComponent implements OnInit {
 
+  vacancieyDetailslevel: VacancyLevel;
+  vacancieyDetailsType: VacancyType;
+  vacancieyDetailsEnterprise: EnterpriseDetails;
   vacancies: Vacancy[];
   enterprises: Enterprise[];
   searchWord: any;
-
-  constructor(private vacancyServiec: VacancyService, private enterpriseService: EnterpriseService, private activeRoute: ActivatedRoute) {
+  vacancieyDetails: Vacancy;
+  constructor(private vacancyServiec: VacancyService, private enterpriseService: EnterpriseService,
+     private typeService: VacancyTypeService, private levelsService: VacancyLevelService,
+     private activeRoute: ActivatedRoute) {
 
    }
   ngOnInit() {
     const id = this.activeRoute.snapshot.params['id'];
     console.log(id);
-    // similar vacancies
+    this.vacancieyDetails = this.vacancyServiec.getById(id);
+    console.log(this.vacancieyDetails);
+    this.vacancieyDetailsEnterprise = this.enterpriseService.getById(this.vacancieyDetails.fK_Enterprise_Id);
+    this.vacancieyDetailsType = this.typeService.getById(this.vacancieyDetails.fK_VacancyType_Id);
+    this.vacancieyDetailslevel = this.levelsService.getById(this.vacancieyDetails.fK_Level_Id);
+
+     // similar vacancies
     this.vacancyServiec.getNotDeleted().subscribe((d) => {this.vacancies = d ; });
-    console.log(this.vacancies);
     const ids = this.vacancies.map(i => i.fK_Enterprise_Id);
     this.enterprises = this.vacancies.map(i => this.enterpriseService.getEnterpriseById(i.fK_Enterprise_Id));
     console.log(this.vacancies);
