0781990387a8f039bdd65b5b0b20ed02eef1dcdd
Marina Saad <Marina_Saad2095@yahoo.com>
2018-06-24 12:26:00 +0200

    home page update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,11 +5,14 @@ import { IOffer } from 'src/app/shared/interfaces/IOffer';
 import { JobOfferService } from 'src/app/shared/services/job-offer.service';
 import { ILogo } from '../shared/interfaces/ILogo';
 import { CompanyLogoService } from'../shared/services/company-logo.service';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [NgbCarouselConfig]  // add NgbCarouselConfig to the component providers
+  
 })
 
 export class HomeComponent implements OnInit {
@@ -17,7 +20,12 @@ export class HomeComponent implements OnInit {
   jobofferitems:IOffer[]
   companylogoitems:ILogo[];
   
-  constructor(private homeservice:HomeService,private offerservice:JobOfferService,private Logoservice:CompanyLogoService ) { }
+  constructor(config: NgbCarouselConfig,private homeservice:HomeService,private offerservice:JobOfferService,private Logoservice:CompanyLogoService ) {
+    // customize default values of carousels used by this component tree
+    config.interval = 10000;
+    config.wrap = false;
+    config.keyboard = false;
+   }
 
   ngOnInit() {
     this.servicessitem=this.homeservice.getAll();
