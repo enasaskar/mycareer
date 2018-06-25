ad3e8d9eeafff31f92c415c91213c50cdc8f0eaf
RanaOsamaAwadallah <rana_osama_awadallah@yahoo.com>
2018-06-25 17:07:59 +0200

    user work and edu subjects

--- src/app/shared/classes/userWorkExperienceModel.ts
+++ src/app/shared/classes/userWorkExperienceModel.ts
@@ -1,12 +1,14 @@
 import { Enterprise } from './enterprise';
 
 export class WorkExperience {
+    userId: number;
     enterpriseID: number;
     content: string;
     startDate: string;
     endDate: string;
 
-    constructor(enterpriseID: number, content: string, startDate: string, endDate: string) {
+    constructor(userId: number, enterpriseID: number, content: string, startDate: string, endDate: string) {
+        this.userId = userId;
         this.enterpriseID = enterpriseID;
         this.content = content;
         this.startDate = startDate;
